26fcbfef7fad5609ae03e05a7f770ce8a28984c5
VHarisop <the.machine.headed@gmail.com>
2015-08-12 01:44:35 +0300

    Added multi-intersect function

--- utils.py
+++ utils.py
@@ -10,9 +10,30 @@ def multi_remove(list_object, *indices):
 
     ''' Removes objects from list at multiple indices.
         The list is modified in place and the removed 
-        items are returned. '''
+        items are returned. 
+        
+        Example: 
+            >> x = [1,4,6,7,8]
+            >> removed = multi_remove(x, 1, 3)
+            >> x
+            [1, 6, 8]
+            >> removed 
+            [7, 4]
+        
+    '''
 
     return list(dq((list.pop(list_object, i)
         for i in sorted(indices, reverse=True)),
         maxlen = len(indices)))
 
+
+def multi_intersection(*lists):
+
+    ''' Returns a set of all items common in all the lists provided.
+        Only works for lists of hashable objects. 
+
+        The time complexity here is [n - 1] * O(L), where:
+            - L is the maximum length of all sets of unique items 
+            - n is the number of lists provided. '''
+
+    return set.intersection(*map(set, lists))
